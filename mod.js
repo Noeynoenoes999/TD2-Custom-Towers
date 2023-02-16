a04eb611b8fc7f36448af035b522701cb267e9a7
SwiftGuard1 <79541415+SwiftGuard1@users.noreply.github.com>
2023-02-15 22:07:04 -0500

    Multiple experimental additions; textures almost working; T4/5 now fire pig projectiles.

--- mod.js
+++ mod.js
@@ -118,6 +118,9 @@ setInterval(function () {
       tower.stats.maxTargets = 10;
       tower.stats.numProjectiles = 1;
       tower.stats.range = 3;
+      tower.birds.forEach((bird) => {
+        bird.projectile = "pig";
+      })
     } else if (tower.type == "bird" && tower.level == 3) {
       tower.stats.dmg = 80;
       tower.stats.fireRate = 1000;
@@ -125,33 +128,36 @@ setInterval(function () {
       tower.stats.maxTargets = 6;
       tower.stats.numProjectiles = 1;
       tower.stats.range = 3;
+      tower.birds.forEach((bird) => {
+        bird.projectile = "pig";
+      });
     } else if (tower.type == "bird") {
-      // tower.birds.forEach((bird) => {
+       tower.birds.forEach((bird) => {
 
-      //   //bird.projectileType = "pig"
+         function base64toBlob(base64Data, contentType) {
+           contentType = contentType || '';
+           var sliceSize = 1024;
+           var byteCharacters = atob(base64Data);
+           var bytesLength = byteCharacters.length;
+           var slicesCount = Math.ceil(bytesLength / sliceSize);
+           var byteArrays = new Array(slicesCount);
 
-      //   function base64toBlob(base64Data, contentType) {
-      //     contentType = contentType || '';
-      //     var sliceSize = 1024;
-      //     var byteCharacters = atob(base64Data);
-      //     var bytesLength = byteCharacters.length;
-      //     var slicesCount = Math.ceil(bytesLength / sliceSize);
-      //     var byteArrays = new Array(slicesCount);
+           for (var sliceIndex = 0; sliceIndex < slicesCount; ++sliceIndex) {
+             var begin = sliceIndex * sliceSize;
+             var end = Math.min(begin + sliceSize, bytesLength);
 
-      //     for (var sliceIndex = 0; sliceIndex < slicesCount; ++sliceIndex) {
-      //       var begin = sliceIndex * sliceSize;
-      //       var end = Math.min(begin + sliceSize, bytesLength);
+             var bytes = new Array(end - begin);
+             for (var offset = begin, i = 0; offset < end; ++i, ++offset) {
+               bytes[i] = byteCharacters[offset].charCodeAt(0);
+             }
+             byteArrays[sliceIndex] = new Uint8Array(bytes);
+           }
+           return new Blob(byteArrays, { type: contentType });
+         }
 
-      //       var bytes = new Array(end - begin);
-      //       for (var offset = begin, i = 0; offset < end; ++i, ++offset) {
-      //         bytes[i] = byteCharacters[offset].charCodeAt(0);
-      //       }
-      //       byteArrays[sliceIndex] = new Uint8Array(bytes);
-      //     }
-      //     return new Blob(byteArrays, { type: contentType });
-      //   }
+         bird.texture.source[0].source.src = URL.createObjectURL(base64toBlob("/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBwgHBgkIBwgKCgkLDRYPDQwMDRsUFRAWIB0iIiAdHx8kKDQsJCYxJx8fLT0tMTU3Ojo6Iys/RD84QzQ5OjcBCgoKDQwNGg8PGjclHyU3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3N//AABEIAIIAXgMBIgACEQEDEQH/xAAcAAEAAgIDAQAAAAAAAAAAAAAABgcEBQECAwj/xAA+EAABAwMCAwQHBAcJAAAAAAABAAIDBAUREiEGEzFBYXGhBxQiUYGRsRUyUvAWIyRCwdHhJjM2Q1NUZHSD/8QAFwEBAQEBAAAAAAAAAAAAAAAAAAIBA//EABkRAQEBAQEBAAAAAAAAAAAAAAABEQIxEv/aAAwDAQACEQMRAD8AvFERAREQFwTgLlYl2mNPbKuYDeOF7h8AUGB+lFlLi0VzCWnc6XY+eMLcg5CpaHaEZ3x5q5oH8yGN+Mamg48QqsxkuvRERS0REQEREBERAWj4yqY4OH6tpkYHyMDGtJ3OTg4HhlbxQz0hUtHFbo6gUsIqnzgc7QNf3XdvVbPSoTF/d+OforcttVDPBGyJ2XNY3U0gtI+B3VSQD9W389isrhiy0VtooaqBsz6iaFpfJLM6R24BIGo4aM42GBsFXaeW+RY9vrILjQ09bSP109RE2WJ+CNTHAEHB7ishQoREQEyhOOqre1cQ1xukT5ap76eSUZY8kgNcf4BbJrNWQiIsaKDekybDKGHI3L34+QH1Km0rXPjc1ryxxBAcACR37qsONoqmG7CKqrpKvEQczUwN5YJO23h3KufWXxqYQdDQPgpVRU/GVTbYH081C2mlhaWNNQ4ODCNv8vrjvUXjHst8VO+F7fc44KWqqK+H1QwhzaeJj9wW7Bxc4gYznYDce7ZV0yO3oyiqo+CLT61NHI11Ox0AZGWaIi0aWnJ3Pft1+JlKwLDWUtwslBW0EfLpKinjkhjwBoYWghuBsMDbCz1zUIiIMO8Ter2qsmHWOB7h8GlVLTghpAPu3Vl8YzNisFUzI1ytDGsyMuyQDj4ZVaMywEOHjuunCeluWup9bt9NUf6sTXb94WUq9tfEdVRUUdLE+m0szu+NxIySex3f7l7u4nuDm4FXTNP4hFv5lT81up2qz4/H9oz/ANZn1ctl9vXfTkVkDh3RtUfvHrlbVvq6qUTPLQ0aWgYaOwYHiq55srLWGxoyTgeCkds4avFVa4Z4L5HFFJHqZDyX+yPdnmY8vgo6zOehWyY+4Q00XqV0q6ctGdIl1NA640uyMfBb1N8ZEs9G1LLS8FWkTTyScymjla2QAcprmghgx2Ae/fdSdQMX+8/7iEf+YXeK/wB5Ds8yCTH7rmDHkQVHzVanKKJW3jemlrTRXOlmop8nluILmSgdoI6KVQyxzRiSJ7XsPRzTkFTjUM9K9qFVwzPcYpZ4qqhbrjMUpaHNLhqDgOu2T4qixdLkBhtdPjveV9K8V0xrOGbrTAZMtHK0DfrpOOi+f+HLQKoet1Lf2du7W/j/AKKuWV7W+w3u7Q09fNDdJockwSQxufuDuds4/itoKe4xNL301/eAMlz45QPIBXVw/Sijs1HBp0lsTdQ7zufMrYYT6pigjW1oGllDcSD+Jkp+qyqepqZSS92nGxaQcg94PRXoqz4//wARkf8AGZ9XKuetrLGljce36qO1nEMNNNJTxTmVzCWuydwR2KRMIz1OVM7Ld7HS2qmEzGCpijy4in31e/OPNbbjIrL7QuWsn7Prx2Ecl2D5L0jrLvJkQ2u4PIGcNp3k+QVvcD3g3zhe31ss7JaowMbVFpG0waNQIHQ5Ocd63uFP3VYqmy8E8RXGsiqbzNHb4I92MZiSU58h5+Csmz22G1UQpoHSvGouc+V2XOJ7T07uxZyKbdbjq9gewtcMggghVFaqDmV8NAxpwJGxua1v3Wg4PgrfXgyipY5ObHTQsk3OtsYB370lxmPYADoFyiLGuksjIo3SSODWNGST0AVW8aV9DcL2+S31TKjlxNZKWEkNO5x8irUIyoF6SaZjJaKZrWgua9riAN8EEZ+ZVc+sviLRgbHfqpjZrZwrVU1LTzWyhdWyxAuLomOc9+nLjnffqd1DoSNLT+eikNl4o4ZtNBGyGmpWVQiDZpYjEDI7tJOcnJyVXTOUx4VtjrPw7brdLoMtPTsZKY/ul4A1Ed2craqOej26OuvCFsnmfLJUNgZHPJI0jXIGjLgT97PvG2c96ka5qEREBERAREQFEvSNHrs8Eg/cnHb72lS1RTjazxTW6auE1WJYcPDBUPMZ3wcszjofLK2elQOADls2/OFatqjp5rPSFrInh0DAcAYPshVVDswduP5KyeHbK63/ALVNXT1UssYHtNaxoHXo0bnpuc93Uqu08s7h+2/Y9joLbzOaaSnZCZMY1lrQCcdmTutgiKFCIiAiIgIiICwL7Dz7NWxAZLoHgDGcnSVnrq9oexzXDIIwUFMQ7x7ducfJXJTNcyniY4Yc1gB+Sh36A6Zf1Vy0w9jTB7Q+OrHkpqOirq6yTHKIiloiIgIiICIiAiIgIiICIiAiIg//2Q==", "image/jpeg"));
 
-      //   bird.texture.source[0].source.src = createObjectURL(base64toBlob("/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBwgHBgkIBwgKCgkLDRYPDQwMDRsUFRAWIB0iIiAdHx8kKDQsJCYxJx8fLT0tMTU3Ojo6Iys/RD84QzQ5OjcBCgoKDQwNGg8PGjclHyU3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3N//AABEIAIIAXgMBIgACEQEDEQH/xAAcAAEAAgIDAQAAAAAAAAAAAAAABgcEBQECAwj/xAA+EAABAwMCAwQHBAcJAAAAAAABAAIDBAUREiEGEzFBYXGhBxQiUYGRsRUyUvAWIyRCwdHhJjM2Q1NUZHSD/8QAFwEBAQEBAAAAAAAAAAAAAAAAAAIBA//EABkRAQEBAQEBAAAAAAAAAAAAAAABEQIxEv/aAAwDAQACEQMRAD8AvFERAREQFwTgLlYl2mNPbKuYDeOF7h8AUGB+lFlLi0VzCWnc6XY+eMLcg5CpaHaEZ3x5q5oH8yGN+Mamg48QqsxkuvRERS0REQEREBERAWj4yqY4OH6tpkYHyMDGtJ3OTg4HhlbxQz0hUtHFbo6gUsIqnzgc7QNf3XdvVbPSoTF/d+OforcttVDPBGyJ2XNY3U0gtI+B3VSQD9W389isrhiy0VtooaqBsz6iaFpfJLM6R24BIGo4aM42GBsFXaeW+RY9vrILjQ09bSP109RE2WJ+CNTHAEHB7ishQoREQEyhOOqre1cQ1xukT5ap76eSUZY8kgNcf4BbJrNWQiIsaKDekybDKGHI3L34+QH1Km0rXPjc1ryxxBAcACR37qsONoqmG7CKqrpKvEQczUwN5YJO23h3KufWXxqYQdDQPgpVRU/GVTbYH081C2mlhaWNNQ4ODCNv8vrjvUXjHst8VO+F7fc44KWqqK+H1QwhzaeJj9wW7Bxc4gYznYDce7ZV0yO3oyiqo+CLT61NHI11Ox0AZGWaIi0aWnJ3Pft1+JlKwLDWUtwslBW0EfLpKinjkhjwBoYWghuBsMDbCz1zUIiIMO8Ter2qsmHWOB7h8GlVLTghpAPu3Vl8YzNisFUzI1ytDGsyMuyQDj4ZVaMywEOHjuunCeluWup9bt9NUf6sTXb94WUq9tfEdVRUUdLE+m0szu+NxIySex3f7l7u4nuDm4FXTNP4hFv5lT81up2qz4/H9oz/ANZn1ctl9vXfTkVkDh3RtUfvHrlbVvq6qUTPLQ0aWgYaOwYHiq55srLWGxoyTgeCkds4avFVa4Z4L5HFFJHqZDyX+yPdnmY8vgo6zOehWyY+4Q00XqV0q6ctGdIl1NA640uyMfBb1N8ZEs9G1LLS8FWkTTyScymjla2QAcprmghgx2Ae/fdSdQMX+8/7iEf+YXeK/wB5Ds8yCTH7rmDHkQVHzVanKKJW3jemlrTRXOlmop8nluILmSgdoI6KVQyxzRiSJ7XsPRzTkFTjUM9K9qFVwzPcYpZ4qqhbrjMUpaHNLhqDgOu2T4qixdLkBhtdPjveV9K8V0xrOGbrTAZMtHK0DfrpOOi+f+HLQKoet1Lf2du7W/j/AKKuWV7W+w3u7Q09fNDdJockwSQxufuDuds4/itoKe4xNL301/eAMlz45QPIBXVw/Sijs1HBp0lsTdQ7zufMrYYT6pigjW1oGllDcSD+Jkp+qyqepqZSS92nGxaQcg94PRXoqz4//wARkf8AGZ9XKuetrLGljce36qO1nEMNNNJTxTmVzCWuydwR2KRMIz1OVM7Ld7HS2qmEzGCpijy4in31e/OPNbbjIrL7QuWsn7Prx2Ecl2D5L0jrLvJkQ2u4PIGcNp3k+QVvcD3g3zhe31ss7JaowMbVFpG0waNQIHQ5Ocd63uFP3VYqmy8E8RXGsiqbzNHb4I92MZiSU58h5+Csmz22G1UQpoHSvGouc+V2XOJ7T07uxZyKbdbjq9gewtcMggghVFaqDmV8NAxpwJGxua1v3Wg4PgrfXgyipY5ObHTQsk3OtsYB370lxmPYADoFyiLGuksjIo3SSODWNGST0AVW8aV9DcL2+S31TKjlxNZKWEkNO5x8irUIyoF6SaZjJaKZrWgua9riAN8EEZ+ZVc+sviLRgbHfqpjZrZwrVU1LTzWyhdWyxAuLomOc9+nLjnffqd1DoSNLT+eikNl4o4ZtNBGyGmpWVQiDZpYjEDI7tJOcnJyVXTOUx4VtjrPw7brdLoMtPTsZKY/ul4A1Ed2craqOej26OuvCFsnmfLJUNgZHPJI0jXIGjLgT97PvG2c96ka5qEREBERAREQFEvSNHrs8Eg/cnHb72lS1RTjazxTW6auE1WJYcPDBUPMZ3wcszjofLK2elQOADls2/OFatqjp5rPSFrInh0DAcAYPshVVDswduP5KyeHbK63/ALVNXT1UssYHtNaxoHXo0bnpuc93Uqu08s7h+2/Y9joLbzOaaSnZCZMY1lrQCcdmTutgiKFCIiAiIgIiICwL7Dz7NWxAZLoHgDGcnSVnrq9oexzXDIIwUFMQ7x7ducfJXJTNcyniY4Yc1gB+Sh36A6Zf1Vy0w9jTB7Q+OrHkpqOirq6yTHKIiloiIgIiICIiAiIgIiICIiAiIg//2Q==", "image/jpeg"));
+         bird.texture.source[0].source.currentSrc = URL.createObjectURL(base64toBlob("/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBwgHBgkIBwgKCgkLDRYPDQwMDRsUFRAWIB0iIiAdHx8kKDQsJCYxJx8fLT0tMTU3Ojo6Iys/RD84QzQ5OjcBCgoKDQwNGg8PGjclHyU3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3N//AABEIAIIAXgMBIgACEQEDEQH/xAAcAAEAAgIDAQAAAAAAAAAAAAAABgcEBQECAwj/xAA+EAABAwMCAwQHBAcJAAAAAAABAAIDBAUREiEGEzFBYXGhBxQiUYGRsRUyUvAWIyRCwdHhJjM2Q1NUZHSD/8QAFwEBAQEBAAAAAAAAAAAAAAAAAAIBA//EABkRAQEBAQEBAAAAAAAAAAAAAAABEQIxEv/aAAwDAQACEQMRAD8AvFERAREQFwTgLlYl2mNPbKuYDeOF7h8AUGB+lFlLi0VzCWnc6XY+eMLcg5CpaHaEZ3x5q5oH8yGN+Mamg48QqsxkuvRERS0REQEREBERAWj4yqY4OH6tpkYHyMDGtJ3OTg4HhlbxQz0hUtHFbo6gUsIqnzgc7QNf3XdvVbPSoTF/d+OforcttVDPBGyJ2XNY3U0gtI+B3VSQD9W389isrhiy0VtooaqBsz6iaFpfJLM6R24BIGo4aM42GBsFXaeW+RY9vrILjQ09bSP109RE2WJ+CNTHAEHB7ishQoREQEyhOOqre1cQ1xukT5ap76eSUZY8kgNcf4BbJrNWQiIsaKDekybDKGHI3L34+QH1Km0rXPjc1ryxxBAcACR37qsONoqmG7CKqrpKvEQczUwN5YJO23h3KufWXxqYQdDQPgpVRU/GVTbYH081C2mlhaWNNQ4ODCNv8vrjvUXjHst8VO+F7fc44KWqqK+H1QwhzaeJj9wW7Bxc4gYznYDce7ZV0yO3oyiqo+CLT61NHI11Ox0AZGWaIi0aWnJ3Pft1+JlKwLDWUtwslBW0EfLpKinjkhjwBoYWghuBsMDbCz1zUIiIMO8Ter2qsmHWOB7h8GlVLTghpAPu3Vl8YzNisFUzI1ytDGsyMuyQDj4ZVaMywEOHjuunCeluWup9bt9NUf6sTXb94WUq9tfEdVRUUdLE+m0szu+NxIySex3f7l7u4nuDm4FXTNP4hFv5lT81up2qz4/H9oz/ANZn1ctl9vXfTkVkDh3RtUfvHrlbVvq6qUTPLQ0aWgYaOwYHiq55srLWGxoyTgeCkds4avFVa4Z4L5HFFJHqZDyX+yPdnmY8vgo6zOehWyY+4Q00XqV0q6ctGdIl1NA640uyMfBb1N8ZEs9G1LLS8FWkTTyScymjla2QAcprmghgx2Ae/fdSdQMX+8/7iEf+YXeK/wB5Ds8yCTH7rmDHkQVHzVanKKJW3jemlrTRXOlmop8nluILmSgdoI6KVQyxzRiSJ7XsPRzTkFTjUM9K9qFVwzPcYpZ4qqhbrjMUpaHNLhqDgOu2T4qixdLkBhtdPjveV9K8V0xrOGbrTAZMtHK0DfrpOOi+f+HLQKoet1Lf2du7W/j/AKKuWV7W+w3u7Q09fNDdJockwSQxufuDuds4/itoKe4xNL301/eAMlz45QPIBXVw/Sijs1HBp0lsTdQ7zufMrYYT6pigjW1oGllDcSD+Jkp+qyqepqZSS92nGxaQcg94PRXoqz4//wARkf8AGZ9XKuetrLGljce36qO1nEMNNNJTxTmVzCWuydwR2KRMIz1OVM7Ld7HS2qmEzGCpijy4in31e/OPNbbjIrL7QuWsn7Prx2Ecl2D5L0jrLvJkQ2u4PIGcNp3k+QVvcD3g3zhe31ss7JaowMbVFpG0waNQIHQ5Ocd63uFP3VYqmy8E8RXGsiqbzNHb4I92MZiSU58h5+Csmz22G1UQpoHSvGouc+V2XOJ7T07uxZyKbdbjq9gewtcMggghVFaqDmV8NAxpwJGxua1v3Wg4PgrfXgyipY5ObHTQsk3OtsYB370lxmPYADoFyiLGuksjIo3SSODWNGST0AVW8aV9DcL2+S31TKjlxNZKWEkNO5x8irUIyoF6SaZjJaKZrWgua9riAN8EEZ+ZVc+sviLRgbHfqpjZrZwrVU1LTzWyhdWyxAuLomOc9+nLjnffqd1DoSNLT+eikNl4o4ZtNBGyGmpWVQiDZpYjEDI7tJOcnJyVXTOUx4VtjrPw7brdLoMtPTsZKY/ul4A1Ed2craqOej26OuvCFsnmfLJUNgZHPJI0jXIGjLgT97PvG2c96ka5qEREBERAREQFEvSNHrs8Eg/cnHb72lS1RTjazxTW6auE1WJYcPDBUPMZ3wcszjofLK2elQOADls2/OFatqjp5rPSFrInh0DAcAYPshVVDswduP5KyeHbK63/ALVNXT1UssYHtNaxoHXo0bnpuc93Uqu08s7h+2/Y9joLbzOaaSnZCZMY1lrQCcdmTutgiKFCIiAiIgIiICwL7Dz7NWxAZLoHgDGcnSVnrq9oexzXDIIwUFMQ7x7ducfJXJTNcyniY4Yc1gB+Sh36A6Zf1Vy0w9jTB7Q+OrHkpqOirq6yTHKIiloiIgIiICIiAiIgIiICIiAiIg//2Q==", "image/jpeg"));
 
       //   // TODO:  Figure out how to load this image as blob or pregenerate it as a blob, otherwise figur eout how to spoof fetch request from the same domain
       //   // fetch(
@@ -163,7 +169,7 @@ setInterval(function () {
       //   //   .then(function (blob) {
 
       //   //   });
-      // });
+       });
 
       tower.stats.dmg = 40;
       tower.stats.fireRate = 125;
